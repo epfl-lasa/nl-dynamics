ab9bf6a7a6222d37c7e27661dafde2397f3595a4
Felix Duvallet <felix.duvallet@epfl.ch>
2015-06-03 16:10:40 +0200

    Added demonstration vector message: going to be overwritten by something else later.

--- src/catkin_ws/src/controller_demonstrations/src/demonstrations/send_one_demonstration.py
+++ src/catkin_ws/src/controller_demonstrations/src/demonstrations/send_one_demonstration.py
@@ -4,27 +4,41 @@ import numpy as np
 
 import rospy
 from std_msgs.msg import String
-from controller_demonstrations.msg import Demonstration
+from controller_demonstrations.msg import Demonstration, Demonstration_vec
 
 class DemonstrationPublisher(object):
     def __init__(self):
-        self.pub = rospy.Publisher('nl_demonstrations', Demonstration, queue_size=10)
+        self.pub = rospy.Publisher('nl_demonstrations', Demonstration_vec, queue_size=10)
         rospy.init_node('send_one_demonstration', anonymous=True)
 
-
-    def make_demo(self, x, y, dx, dy):
+    def make_demo_point(self, x, y, dx, dy):
         demo = Demonstration(x, y, dx, dy)
         return demo
 
+    def make_demo_vec(self, data):
+        vec = Demonstration_vec()
+        for d in data:
+            point = self.make_demo_point(*d)
+            vec.points.append(point)
+            vec.num_points += 1
+        return vec
+
+
     def run(self):
-        d = self.make_demo(0, -1, 2, 1)
-        self.pub.publish(d)
-        log_str = "Sent demonstration at t={} -- {}".format(rospy.get_time(), d)
+        data = [(0, 1, 2, 3), (1, 2, 3, 4)]
+        v = self.make_demo_vec(data)
+        self.pub.publish(v)
+
+        log_str = "Sent demonstration at t={} -- {}".format(rospy.get_time(), v)
         rospy.loginfo(log_str)
+
+
+
 
 if __name__ == '__main__':
     try:
         demo_publisher = DemonstrationPublisher()
         demo_publisher.run()
-    except rospy.ROSInterruptException:
+    except rospy.ROSInterruptException as e:
+        print e
         pass
