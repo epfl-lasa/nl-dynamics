752a19fa38af1de9569a889f0c861d5e0ae310aa
Felix Duvallet <felix.duvallet@epfl.ch>
2015-06-03 10:45:48 +0200

    first pass at argument parsing

--- src/cpp/GPMDS/src/expData.cpp
+++ src/cpp/GPMDS/src/expData.cpp
@@ -1,9 +1,12 @@
-#include "GPMDS.h"
-#include "Timer.h"
 #include <iostream>
 #include <eigen3/Eigen/Dense>
 #include <vector>
 #include <fstream>
+#include <getopt.h>
+
+#include "GPMDS.h"
+#include "Timer.h"
+
 
 using namespace std;
 using namespace Eigen;
@@ -26,7 +29,7 @@ Vector3r linear_isotropic_dynamics(Vector3r pos) {
   return vel;
 }
 
-int main(int argc, char *argv[]) {
+int main2(int argc, char *argv[]) {
   const double ell = 0.07;
   const double sigmaF = 1.0;
   const double sigmaN = 0.4;
@@ -55,4 +58,78 @@ int main(int argc, char *argv[]) {
   cout<<"reshaping took.."<<tmr.elapsed()<<endl;
 
   return 0;
+}
+
+static int verbose_flag;
+int
+main (int argc, char **argv)
+{
+  int c;
+  string filename = "data.txt";
+
+  while (1)
+    {
+      static struct option long_options[] =
+        {
+          /* These options set a flag. */
+          {"verbose", no_argument,       &verbose_flag, 1},
+          /* These options don’t set a flag.
+             We distinguish them by their indices. */
+          {"file",    required_argument, 0, 'f'},
+          {0}
+        };
+      /* getopt_long stores the option index here. */
+      int option_index = 0;
+
+      c = getopt_long (argc, argv, "f:",
+                       long_options, &option_index);
+
+      /* Detect the end of the options. */
+      if (c == -1)
+        break;
+
+      switch (c)
+        {
+        case 0:
+          /* If this option set a flag, do nothing else now. */
+          if (long_options[option_index].flag != 0)
+            break;
+          printf ("option %s", long_options[option_index].name);
+          if (optarg)
+            printf (" with arg %s", optarg);
+          printf ("\n");
+          break;
+
+        case 'f':
+          printf ("option -f with value `%s'\n", optarg);
+          filename = optarg;
+          break;
+
+        case '?':
+          /* getopt_long already printed an error message. */
+          break;
+
+        default:
+          abort ();
+        }
+    }
+
+  /* Instead of reporting ‘--verbose’
+     and ‘--brief’ as they are encountered,
+     we report the final status resulting from them. */
+  if (verbose_flag)
+    puts ("verbose flag is set");
+
+  /* Print any remaining command line arguments (not options). */
+  if (optind < argc)
+    {
+      printf ("non-option ARGV-elements: ");
+      while (optind < argc)
+        printf ("%s ", argv[optind++]);
+      putchar ('\n');
+    }
+
+  cout << "Filename: " << filename << endl;
+
+  exit (0);
 }
