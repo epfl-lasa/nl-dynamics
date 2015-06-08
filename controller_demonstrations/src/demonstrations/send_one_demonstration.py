5bb685cf29c108b2ea6de7f64b26af8f9b7040ee
Felix Duvallet <felix.duvallet@epfl.ch>
2015-06-08 17:14:45 +0200

    Demonstration sender:  - List bag files in directory  - Load rosbag from files  - Command-line flags  - Use new message type.  - Subscribe to Kuka state message.

--- controller_demonstrations/src/demonstrations/send_one_demonstration.py
+++ controller_demonstrations/src/demonstrations/send_one_demonstration.py
@@ -1,24 +1,33 @@
 #! /usr/bin/env python
 
+import sys
+import argparse
 import numpy as np
 import time
 import os
 
 import rospy
-from std_msgs.msg import String
-from controller_demonstrations.msg import Demonstration, Demonstration_vec
+import rosbag
+from nl_msgs.msg import CartStateStamped
+from nl_msgs.msg import AnchoredDemonstration
 
 import file_util
 
-class DemonstrationPublisher(object):
-    def __init__(self, demonstration_dir='data_demonstrations'):
-        self.pub = rospy.Publisher('nl_demonstrations', Demonstration_vec, queue_size=10)
-        rospy.init_node('send_one_demonstration', anonymous=True)
+class PublishCorrections(object):
+    channel_corrections = 'nl_corrections'
+    channel_kuka_state = 'KUKA/CartState'
 
-        # Demonstrations are dict word->data
-        self._demonstrations = {}
+    def __init__(self, demonstration_dir):
+        rospy.init_node('publish_corrections', anonymous=True)
+        self.pub = rospy.Publisher(PublishCorrections.channel_corrections,
+                                   AnchoredDemonstration, queue_size=10)
+        rospy.Subscriber(PublishCorrections.channel_kuka_state,
+                         CartStateStamped, self.kuka_callback)
 
-        base = file_util.get_base()
+        # Corrections are dict word->data
+        self._corrections = {}
+
+        # Load demonstrations
         self.load_all_demonstrations(demonstration_dir)
 
 
@@ -27,33 +36,47 @@ class DemonstrationPublisher(object):
         ret = {}
 
         #TODO: iterate over files in
-        file_util.get_files(file_util.get_base(), demonstration_dir)
+        files = file_util.get_files('', demonstration_dir)
+        for f in files:
+            filepath = file_util.get_fpath(demonstration_dir, f)
+            anchored_demo = self.load_demonstration(filepath)
 
+            words = anchored_demo.words
+            for w in words:
+                if w in ret:
+                    rospy.logwarn('Word [{}] already in demonstration. '
+                        ' Overwriting with data from: {}.'.format(w, f))
+                ret[w] = anchored_demo.demonstration
+
+        rospy.loginfo('Have {} total demonstrations: {}'.format(
+            len(ret.keys()), ret.keys()))
+        return ret
+
+    def load_demonstration(self, filepath):
+        # Load a single demonstration. Returns a single AnchoredDemonstration message.
+        rospy.loginfo('Loading demonstration from {}.'.format(filepath))
+
+        msg = None
+        with rosbag.Bag(filepath) as bag:
+            assert bag.get_message_count() == 1, 'Demonstration should have exactly one message'
+            for (_, msg, _) in bag.read_messages():  # Get the message contents.
+                pass
+
+        assert msg is not None
+
+        rospy.loginfo('  Loaded {} demonstration points, words: {}'.format(
+            msg.num_points, msg.words))
+
+        return msg
+
+    def send_correction(self, command):
         pass
 
-
-
-    def make_demo_point(self, x, y, dx, dy):
-        demo = Demonstration(x, y, dx, dy)
-        return demo
-
-    def make_demo_vec(self, data):
-        vec = Demonstration_vec()
-        for d in data:
-            point = self.make_demo_point(*d)
-            vec.points.append(point)
-            vec.num_points += 1
-        return vec
-
-    def load_demonstration(self, word):
+    def kuka_callback(self, data):
+        print 'Received kuka state'
         pass
 
-    def send_demonstrations(self, command):
-        pass
-
-
-
-    def run(self):
+    def do(self):
         data = [(0, 1, 2, 3), (1, 2, 3, 4)]
         v = self.make_demo_vec(data)
         self.pub.publish(v)
@@ -64,10 +87,25 @@ class DemonstrationPublisher(object):
         time.sleep(0.5)
 
 
-if __name__ == '__main__':
+def run(arguments):
+    parser = argparse.ArgumentParser(
+        description=('Load a directory of demonstration data (stored as bag '
+                     'files).'))
+    parser.add_argument('--demo_dir', metavar='directory', required=True)
+    args = parser.parse_args(arguments)
+
+
+    demo_publisher = PublishCorrections(args.demo_dir)
+
+    return
+
     try:
-        demo_publisher = DemonstrationPublisher()
         demo_publisher.run()
     except rospy.ROSInterruptException as e:
         print e
         pass
+
+
+if __name__ == '__main__':
+    arguments = sys.argv[1:]  # argv[0] is the program name.
+    run(arguments)
