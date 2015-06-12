9ea461bb7f980ee0b40861445370cb58872090bd
Felix Duvallet <felix.duvallet@epfl.ch>
2015-06-12 13:31:46 +0200

    Enables tab-completion for multiple demonstrations.

--- controller_demonstrations/src/demonstrations/send_multiple_demonstrations.py
+++ controller_demonstrations/src/demonstrations/send_multiple_demonstrations.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 import sys
 import argparse
-import time
+import readline
 
 import rospy
 
@@ -19,12 +19,37 @@ def is_finished(command, allow_single_letter):
     finished = False
 
     if command == 'quit': finished = True
-    if command == 'done': finished = True
+    if command == 'stop': finished = True
 
     if allow_single_letter:
         if command == 'q': finished = True
 
     return finished
+
+class SimpleCompleter(object):
+    # Tab-completion for a set of possible choices.
+    def __init__(self, choices):
+        self.choices = sorted(choices)
+        return
+
+    def complete(self, text, state):
+        response = None
+        if state == 0:
+            # This is the first time for this text, so build a match list.
+            if text:
+                self.matches = [s
+                                for s in self.choices
+                                if s and s.startswith(text)]
+            else:
+                self.matches = self.choices[:]
+
+        # Return the state'th item from the match list,
+        # if we have that many.
+        try:
+            response = self.matches[state]
+        except IndexError:
+            response = None
+        return response
 
 
 def get_command(allow_single_letter):
@@ -44,6 +69,14 @@ def run_send_multiple_commands(arguments):
     args = parser.parse_args(arguments)
 
     demo_publisher = PublishCorrections(args.demo_dir)
+
+    words = demo_publisher.words()
+    words.extend(['stop', 'quit'])
+
+    # Use the tab key for completion, using the words in the demo publisher
+    # (along with stop/quit).
+    readline.set_completer(SimpleCompleter(words).complete)
+    readline.parse_and_bind('tab: complete')
 
     try:
         finished = False
