cd8fdf5a4b9384746a462aaf8a4fe8a177e773f9
Felix Duvallet <felix.duvallet@epfl.ch>
2015-06-22 09:46:37 +0200

    filled in some of the demo_sandbox visualization.

--- controller_demonstrations/src/demonstrations/visualize_demo_sandbox.py
+++ controller_demonstrations/src/demonstrations/visualize_demo_sandbox.py
@@ -7,11 +7,12 @@ import time
 import rospy
 from geometry_msgs.msg import Point
 from visualization_msgs.msg import Marker
+from visualization_msgs.msg import MarkerArray
 
 from correction_publisher import PublishCorrections
 from nl_msgs.msg import AnchoredDemonstration
 
-channel_viz = 'visualization_channel'
+channel_viz = 'visualization_marker_array'
 
 
 def run(arguments):
@@ -28,11 +29,30 @@ def run(arguments):
 
     pass
 
-def create_correction_viz_msg(anchored_correction):
+def create_correction_viz_msg(anchored_correction, id=0):
 
     marker = Marker()
 
-    # TODO(Felix) Fill in marker base stuff
+    marker.header.frame_id = "/my_frame"
+    marker.header.stamp = rospy.Time.now()
+    marker.ns = "my_namespace"
+    marker.id = id
+    marker.type = Marker.LINE_STRIP
+    marker.action = Marker.ADD
+    marker.pose.position.x = 1
+    marker.pose.position.y = 0
+    marker.pose.position.z = 0
+    marker.pose.orientation.x = 0.0
+    marker.pose.orientation.y = 0.0
+    marker.pose.orientation.z = 0.0
+    marker.pose.orientation.w = 1.0
+    marker.scale.x = 0.1
+    marker.scale.y = 0
+    marker.scale.z = 0
+    marker.color.a = 1.0  # Alpha must not be zero.
+    marker.color.r = 0.0
+    marker.color.g = 1.0
+    marker.color.b = 1.0
 
     for cart_state in anchored_correction:
         point = cart_state_to_point(cart_state)
@@ -48,26 +68,31 @@ def cart_state_to_point(point):
 
 def do(demo_dir):
 
-    # Use the correction publisher to load the demonstrations for a directory.
-
-
     rospy.init_node('visualization_node', anonymous=False)
-    publisher = rospy.Publisher(channel_viz, Marker, queue_size=10)
+    publisher = rospy.Publisher(channel_viz, MarkerArray, queue_size=10)
 
     time.sleep(1.0)
 
+    marker_array = MarkerArray()
+
+    # Use the correction publisher to load the demonstrations for a directory.
     corrections = PublishCorrections.load_all_demonstrations(demo_dir)
 
-    for (word, correction) in corrections.iteritems():
+    for (idx, (word, correction)) in enumerate(corrections.iteritems()):
         # Correction is a list[CartStateStamped]
         print 'word: ', word
 
-        marker = create_correction_viz_msg(correction)
+        marker = create_correction_viz_msg(correction, idx)
         print '  Got a marker with {} points'.format(len(marker.points))
 
-        # Publish each message
-        publisher.publish(marker)
+        marker_array.markers.append(marker)
+
         pass
+
+
+    # Publish the entire marker array
+    publisher.publish(marker_array)
+    rospy.loginfo('Published markers: {}'.format(len(marker_array.markers)))
 
     publisher.unregister()
 
