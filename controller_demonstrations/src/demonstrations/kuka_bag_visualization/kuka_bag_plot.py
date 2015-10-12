f585c7f41cc88a4c557557a58b2a1042e7306ba1
sballmer <stephane.ballmer@epfl.ch>
2015-10-12 16:21:45 +0200

    rearrange function, and use structure to get classifie data in a better way

--- controller_demonstrations/src/demonstrations/kuka_bag_visualization/kuka_bag_plot.py
+++ controller_demonstrations/src/demonstrations/kuka_bag_visualization/kuka_bag_plot.py
@@ -5,13 +5,21 @@ import rosbag
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
+# Data structur
+class Vector3D:
+    x=[]
+    y=[]
+    z=[]
+
+class PositionData:
+    position = Vector3D()
+    velocity = Vector3D()
 
 def load(name):
     # Open the given file and extract the CartState topics.
     #bag = rosbag.Bag('2015.10.09-kuka.with.correction.01.bag')
     bag = rosbag.Bag(name)
     gen = bag.read_messages(topics=['/KUKA/CartState'])
-    #gen = bag.read_messages(topics=['/KUKA/DesiredState'])
     stuff = list(gen)
 
     # The rosbag tells us the topic, message data (what we want), and time.
@@ -19,28 +27,40 @@ def load(name):
 
     print("Have a trajectory with %d data points" % len(msgs))
 
-    # Extract x/y/z coordinates
-    x = [msg.pose.position.x for msg in msgs]
-    y = [msg.pose.position.y for msg in msgs]
-    z = [msg.pose.position.z for msg in msgs]
-    #x = [msg.twist.linear.x for msg in msgs]
-    #y = [msg.twist.linear.y for msg in msgs]
-    #z = [msg.twist.linear.z for msg in msgs]
+    # create output structure
+    trajectory_data = PositionData()
+    original_data   = PositionData()
 
-    return x, y, z
+    # Extract x/y/z coordinates of the position and the linear velocity between each point
+    trajectory_data.position.x = [msg.pose.position.x for msg in msgs]
+    trajectory_data.position.y = [msg.pose.position.y for msg in msgs]
+    trajectory_data.position.z = [msg.pose.position.z for msg in msgs]
+    trajectory_data.velocity.x = [msg.twist.linear.x for msg in msgs]
+    trajectory_data.velocity.y = [msg.twist.linear.y for msg in msgs]
+    trajectory_data.velocity.z = [msg.twist.linear.z for msg in msgs]
+
+    # Extract x/y/z coordinates of the original Dynamic velocity
+    gen = bag.read_messages(topics=['/KUKA/DesiredState'])
+    stuff = list(gen)
+    [topics, msgs, times] = zip(*stuff)
+    original_data.velocity.x = [msg.twist.linear.x for msg in msgs]
+    original_data.velocity.y = [msg.twist.linear.y for msg in msgs]
+    original_data.velocity.z = [msg.twist.linear.z for msg in msgs]
+
+    # should I take care about the case that len(original_data.velocity) != len(trajectory_data.velocity) ??
+
+    return trajectory_data, original_data
     
-def trace(name):
-	(x, y, z) = load(name)
-
+def plotPoints(data):
 	fig = plt.figure()
 	ax = fig.gca(projection='3d')
 
-	ax.plot(x, y, z, label=name)
+	ax.plot(data.position.x, data.position.y, data.position.z, label='2015.10.09-kuka-no.correction.bag')
 	ax.set_xlabel('x')
 	ax.set_ylabel('y')
 	ax.set_zlabel('z')
 
-	ax.scatter(x[0:1], y[0:1], z[0:1], label='Start')
+	ax.scatter(data.position.x[0:1], data.position.y[0:1], data.position.z[0:1], label='Start')
 
 	ax.legend()
 	plt.ion()
@@ -48,10 +68,13 @@ def trace(name):
 
 
 def run():
-    trace('2015.10.09-kuka.with.correction.01.bag')
-    trace('2015.10.09-kuka.with.correction.02.bag')
-    trace('2015.10.09-kuka.two.corrections.bag')
-    trace('2015.10.09-kuka-no.correction.bag')
+    (trajectory_data, original_data) = load('2015.10.09-kuka-no.correction.bag')
+    plotPoints(trajectory_data)
+
+    # trace('2015.10.09-kuka.with.correction.01.bag')
+    # trace('2015.10.09-kuka.with.correction.02.bag')
+    # trace('2015.10.09-kuka.two.corrections.bag')
+    # trace('2015.10.09-kuka-no.correction.bag')
 	
     raw_input('press enter')
 
