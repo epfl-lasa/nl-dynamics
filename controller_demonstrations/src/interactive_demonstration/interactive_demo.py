6b68052dfc704320bb0eba4abd8bc431bce4e78b
Felix Duvallet <felix.duvallet@epfl.ch>
2015-10-13 11:12:50 +0200

    moved demo_collection machine to its own package.

--- controller_demonstrations/src/interactive_demonstration/interactive_demo.py
+++ controller_demonstrations/src/interactive_demonstration/interactive_demo.py
@@ -7,6 +7,7 @@ import sys
 
 from std_msgs.msg import String
 
+
 class ReadyState(smach.State):
     outcome_ready = 'ready'
     outcome_finished = 'finished'
@@ -87,48 +88,6 @@ class UserInteraction(smach.StateMachine):
 
         pass
 
-
-class DelayState(smach.State):
-    outcome_ok = 'okay'
-    outcomes = [outcome_ok]
-
-    def __init__(self, delay):
-        super(DelayState, self).__init__(outcomes=DelayState.outcomes)
-        self._delay = delay
-
-    def execute(self, user_data):
-        rospy.loginfo('Sleeping for {} seconds...'.format(self._delay))
-        rospy.sleep(self._delay)
-        rospy.loginfo('Finished sleeping')
-        return DelayState.outcome_ok
-
-
-class DemoCollection(smach.StateMachine):
-    outcome_success = 'success'
-    outcome_failure = 'failure'
-    outcomes = [outcome_success, outcome_failure]
-
-    def __init__(self):
-
-        super(DemoCollection, self).__init__(
-            outcomes=DemoCollection.outcomes)
-
-        delay1_name = 'DELAY_1'
-        delay1_state = DelayState(1)
-
-        delay2_name = 'DELAY_2'
-        delay2_state = DelayState(2)
-
-        delay3_name = 'DELAY_3'
-        delay3_state = DelayState(3)
-
-        with self:
-            self.add(delay1_name, delay1_state,
-                     transitions={DelayState.outcome_ok: delay2_name})
-            self.add(delay2_name, delay2_state,
-                     transitions={DelayState.outcome_ok: delay3_name})
-            self.add(delay3_name, delay3_state,
-                     transitions={DelayState.outcome_ok: DemoCollection.outcome_success})
 
 
 def run(arguments):
