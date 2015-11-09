c24799f363b93bada9b990f02506830ce8a7513e
Cyril Schmitt <cyril.schmitt@epfl.ch>
2015-11-09 17:13:36 +0100

    Adds branch for executing a command, in a separate python module.

--- controller_demonstrations/src/interactive_demonstration/interactive_demo.py
+++ controller_demonstrations/src/interactive_demonstration/interactive_demo.py
@@ -10,6 +10,7 @@ import std_msgs
 from demo_collection import DemoCollectionMachine
 from say_state import SayState
 from branch_changingspeed import ChangingSpeedBranch
+from branch_gettingcommand import GettingCommandBranch
 #from branch_changespeed import
 
 
@@ -64,70 +65,7 @@ class ReadyState(smach.State):
         rospy.loginfo('Got message: {}'.format(self.msg))
 
 
-class GetCommand(smach.State):
-    outcome_getcommand = 'getcommand'
-    outcome_unknowncommand = 'unknowncommand'
-    outcome_list = 'list'
-    outcomes = [outcome_getcommand, outcome_unknowncommand, outcome_list]
 
-    def __init__(self, my_list):
-        # specify the outcomes
-        smach.State.__init__(self, outcomes=GetCommand.outcomes)
-        # Subscribe to a Topic
-        topic_sub = '/nl_command_parsed'
-        rospy.Subscriber(topic_sub, std_msgs.msg.String, self.callback, queue_size=1)
-        topic_pub = '/robot_control/desired_command'
-        self.pub = rospy.Publisher(topic_pub, std_msgs.msg.String, queue_size=5)
-        # internal data
-        self.msg = ''
-        self.command_list = my_list
-
-    def command_list_str(self):
-        my_command_list= ' '.join(self.command_list)
-        return my_command_list
-
-    def command_in_dictionnary(self, msg):
-        b = -1
-        cmd = ''
-        msg_split = self.msg.split()  # Separe la string par mot (separateur *espace*
-        length_msg = len(msg_split)  # Retourne le nombre de mots dans la string
-        for i in range(length_msg):  # Parcoure chaque mot
-            if (msg_split[i] in self.command_list):  # Si un des mots est dans la string msg
-                b = i  # Alors il donne la place du mot dans la string
-
-        if (b >= 0):  # empty string is checked here and if the number is in the string also
-            cmd = msg_split[b]  # contient le mot qui est dans le dictionnaire
-            self.pub.publish(cmd)
-            return True
-        else:
-            return False
-
-    def execute(self, userdata):
-        # Assumption for now: wait until the user provides *one* of the available commands. Stay in this state until this is true.
-        # We only return from this state once the user has provided a known command.
-        # TODO: LOGINFO for the command type.
-        # TODO later: after 10 seconds in this state, return outcome_unknowncommand
-        self.msg=''
-        begin=time.time()
-        end=0
-        while ((begin+10)>end):
-            if (self.msg != ''):
-                if (self.command_in_dictionnary(self.msg)):
-                    rospy.loginfo('This command exist yet.')
-                    #pub.publish(self.msg) !!!!!!!!!!!!!
-                    self.msg=''
-                    return GetCommand.outcome_getcommand
-                elif(self.msg=='list'):
-                    return GetCommand.outcome_list
-            end=time.time()
-        rospy.loginfo('This command does not exist yet.')
-        return GetCommand.outcome_unknowncommand
-
-                    # Publish the string to a node where all the commands are registered (Command_Node) which will publish in the Robot_Node to execute it
-                    # Should I make the check before 'if the command exist' ? And so implement the dictionnary when I teach a new command ?
-
-    def callback(self, data):
-        self.msg = data.data
 
 
 class UserInteraction(smach.StateMachine):
@@ -153,28 +91,12 @@ class UserInteraction(smach.StateMachine):
         branchspeed_name = 'SPEED'
         branchspeed_machine = ChangingSpeedBranch()
 
+        branchcommand_name = 'COMMAND'
+        branchcommand_machine = GettingCommandBranch()
+
         finished_state = SayState("I am finished")
         finished_name = 'SAY_FINISHED'
 
-        askcommand_state = SayState('Which command would you like me to do ?')
-        askcommand_name = 'ASK_COMMAND'
-
-        getcommand_state = GetCommand(['left', 'right', 'up', 'down', 'dance'])
-        getcommand_name = 'GET_COMMAND'
-
-
-        # implement something like: getcommand_state.command_list() -> 'left, right, up, down'
-        #  tip: loop up ', '.join() google string join
-        # listing_state = SayState('The available commands are ' + command_list)
-
-        commanddone_state = SayState('Okay I have done your command')
-        commanddone_name = 'COMMAND_DONE'
-
-        unknowncommand_state = SayState('Unknown Command')
-        unknowncommand_name = 'UNKNOWN_COMMAND'
-
-        listing_state = SayState('The list of command is :'+ getcommand_state.command_list_str())
-        listing_name = 'LISTING'
 
         # All states are now defined. Connect them.
         with self:
@@ -192,41 +114,27 @@ class UserInteraction(smach.StateMachine):
                                   ReadyState.outcome_finished: finished_name,
                                   ReadyState.outcome_success: hw_name,
                                   ReadyState.outcome_askingspeed: branchspeed_name,
-                                  ReadyState.outcome_askcommand: askcommand_name})
+                                  ReadyState.outcome_askcommand: branchcommand_name})
 
             # Here the connected state is actually a whole other
             # StateMachine. This is valid as long as its outcomes are properly
             # connected.
             self.add(collect_name, collect_machine,
-                    transitions={
-                         DemoCollectionMachine.outcome_success: hw_name,
-                         # Go back to hello world
-                         DemoCollectionMachine.outcome_failure: UserInteraction.outcome_failure})
+                    transitions={DemoCollectionMachine.outcome_success: hw_name,
+                                DemoCollectionMachine.outcome_failure: UserInteraction.outcome_failure})
 
             self.add(branchspeed_name, branchspeed_machine,
                     transitions={ChangingSpeedBranch.outcome_success: hw_name})
+
+            self.add(branchcommand_name, branchcommand_machine,
+                    transitions={GettingCommandBranch.outcome_success: hw_name})
 
             self.add(finished_name, finished_state,
                     transitions={SayState.outcome_success: UserInteraction.outcome_success})
 
 
             # Giving a command to do States
-            self.add(askcommand_name, askcommand_state,
-                     transitions={SayState.outcome_success: getcommand_name})
 
-            self.add(getcommand_name, getcommand_state,
-                     transitions={GetCommand.outcome_getcommand: commanddone_name,
-                                GetCommand.outcome_unknowncommand: unknowncommand_name #})
-                                ,GetCommand.outcome_list: listing_name})
-
-            self.add(unknowncommand_name, unknowncommand_state,
-                     transitions={SayState.outcome_success: hw_name})
-
-            self.add(commanddone_name, commanddone_state,
-                     transitions={SayState.outcome_success: hw_name})
-
-            self.add(listing_name,listing_state,
-                    transitions={SayState.outcome_success: askcommand_name})
 
         pass
 
