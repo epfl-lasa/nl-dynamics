66dffcf234d6739935e9b0ffc82190f756b945ac
Cyril Schmitt <cyril.schmitt@epfl.ch>
2015-11-17 15:59:33 +0100

    adds modification and implementation of the teaching branch and some minor modifications to the two others

--- controller_demonstrations/src/interactive_demonstration/branch_teachingcommand.py
+++ controller_demonstrations/src/interactive_demonstration/branch_teachingcommand.py
@@ -8,37 +8,69 @@ from say_state import SayState
 
 
 class GetTeachCommand(smach.State):
-    #subscribe to position topic
-    def __init__(self, my_list):
+
+    outcome_commandteached ='commandteached'
+    outcomes = [outcome_commandteached]
+
+    def __init__(self):
+        smach.State.__init__(self, outcomes=GetTeachCommand.outcomes)
+        topic_sub = '/nl_command_parsed'
+        rospy.Subscriber(topic_sub, std_msgs.msg.String, self.callback, queue_size=1)
+        currentcommand=''
 
     def execute(self, userdata):
+        self.cmd=''
+        while(self.cmd==''):
+            rospy.sleep(0.5)
+            return GetTeachCommand.outcome_commandteached
 
     def current_command(self):
-        current_command=self.cmd
-        return self.current_cmd
+        currentcommand=self.cmd
+        return currentcommand
 
     def callback(self, data):
-
+        self.cmd = data.data
 
 
 class RightOrWrong(smach.State):
-    def __init__(self, my_list):
-    	#two outcomes : succes (if right word) failure (if wrong word)
-    	#this will be conclude by the input of the user
+
+    outcome_success ='success'
+    outcome_failure ='failure'
+    outcome_explanation='explanation'
+    outcomes = [outcome_success, outcome_failure, outcome_explanation]
+
+    def __init__(self):
+        smach.State.__init__(self, outcomes=RightOrWrong.outcomes)
+        topic_sub = '/nl_command_parsed'
+        rospy.Subscriber(topic_sub, std_msgs.msg.String, self.callback, queue_size=1)
+
     def execute(self, userdata):
+        if(self.cmd=='Yes'):
+            return RightOrWrong.outcome_success
+        elif(self.cmd=='No'):
+            return RightOrWrong.outcome_failure
+        else:
+            return RightOrWrong.outcome_explanation
 
     def callback(self, data):
-
+        self.cmd=data.data
 
 
 class Demonstration(smach.State):
 
-    def __init__(self, my_list):
+    outcome_demonstration ='Demonstration'
+    outcomes = [outcome_demonstration]
 
+    def __init__(self):
+        smach.State.__init__(self, outcomes=Demonstration.outcomes)
     def execute(self, userdata):
-
+        while(self.cmd!='Start'):
+            rospy.sleep(0.5)
+            while(self.cmd!='Stop'):
+                rospy.sleep(0.5)
+                return Demonstration.outcome_demonstration
     def callback(self, data):
-
+        self.cmd=data.data
 
 
 
@@ -49,8 +81,8 @@ class TeachingCommandMachine(smach.StateMachine):
 
     def __init__(self):
 
-        super(GettingCommandBranch, self).__init__(
-            outcomes=GettingCommandBranch.outcomes)
+        super(TeachingCommandMachine, self).__init__(
+            outcomes=TeachingCommandMachine.outcomes)
 
         askteachcommand_name='Which command ?'
         askteachcommand_state=SayState('Which command would you like to teach me ?')
@@ -59,10 +91,13 @@ class TeachingCommandMachine(smach.StateMachine):
         getteachcommand_state=GetTeachCommand()
 
         confirmteachcommand_name='Right command ?'
-        confirmteachcommand_state=SayState('Are you going to teach ' + GetTeachCommand.current_command)
+        confirmteachcommand_state=SayState('Are you going to teach ' + getteachcommand_state.current_command())
 
         rw_name='Right or Wrong'
         rw_state=RightOrWrong()
+
+        startdemonstration_name='Start Demonstration'
+        startdemonstration_state=SayState('Ok, say Start to begin the recording and Stop to end it')
 
         demonstration_name="Demonstration"
         demonstration_state=Demonstration() #create class
@@ -71,22 +106,35 @@ class TeachingCommandMachine(smach.StateMachine):
         confirmation_name='Confirmation'
         confirmation_state=SayState('Do you want me to save this new command ?')
 
+        explanation_name='Explanation'
+        explanation_state=SayState('Say Yes or No please')
+
         with self:
             self.add(askteachcommand_name, askteachcommand_state,
                      transitions={SayState.outcome_success: getteachcommand_name})
 
             self.add(getteachcommand_name, getteachcommand_state,
-                     transitions={GetTeachCommand.outcome_success: confirmteachcommand_name})
+                     transitions={GetTeachCommand.outcome_commandteached: confirmteachcommand_name})
 
             self.add(confirmteachcommand_name, confirmteachcommand_state,
-                     transitions={SayState.outcome_success: demonstration_name
-                                  SayState.outcome_failure: askteachcommand_name})
+                     transitions={SayState.outcome_success: rw_name})
+
+            self.add(rw_name, rw_state,
+                     transitions={RightOrWrong.outcome_success: startdemonstration_name,
+                                  RightOrWrong.outcome_failure: askteachcommand_name,
+                                  RightOrWrong.outcome_explanation: explanation_name})
+
+            self.add(explanation_name, explanation_state,
+                     transitions={SayState.outcome_success: rw_name})
+
+            self.add(startdemonstration_name, startdemonstration_state,
+                     transitions={SayState.outcome_success: demonstration_name})
 
             self.add(demonstartion_name, demonstration_state,
-                     transitions={SayState.outcome_success: confirmation_name})
+                     transitions={Demonstration.outcome_demonstration: confirmation_name})
 
             self.add(confirmation_name, confirmation_state,
-                     transitions={SayState.outcome_success: TeachingCommandMachine.outcome_succes
+                     transitions={SayState.outcome_success: TeachingCommandMachine.outcome_succes,
                                   SayState.outcome_failure: demonstration_state})
 
 
