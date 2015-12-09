5078a4f6b2d1e891e44b61a4b02709fcfaec28d1
cgschmit <cyrilguy.schmitt@gmail.com>
2015-12-09 14:57:27 +0100

    adds user-data into the teahcing branch

--- controller_demonstrations/src/interactive_demonstration/ConfirmationMachine.py
+++ controller_demonstrations/src/interactive_demonstration/ConfirmationMachine.py
@@ -82,7 +82,6 @@ class ConfirmationMachine(smach.StateMachine):
         listenconfirmation_state = ListenConfirmation()
         listenconfirmation_name = 'Listen Confirmation'
 
-
         with self:
             self.add(listencommand_name, listencommand_state,
                      transitions={ListenCommand.outcome_listened: askconfirmation_name,
@@ -99,6 +98,8 @@ class ConfirmationMachine(smach.StateMachine):
                      transitions={ListenConfirmation.outcome_success: ConfirmationMachine.outcome_success,
                                   ListenConfirmation.outcome_failure: ConfirmationMachine.outcome_failure,
                                   ListenConfirmation.outcome_reset: ConfirmationMachine.outcome_reset})
+                                  
+            
 
 
 if __name__ == '__main__':
