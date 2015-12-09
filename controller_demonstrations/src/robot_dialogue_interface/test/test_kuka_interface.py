867c2fbff02df16df0e1126aab1850628963cf2c
Felix Duvallet <felix.duvallet@epfl.ch>
2015-12-09 11:44:08 +0100

    new unit test stub.

---
+++ controller_demonstrations/src/robot_dialogue_interface/test/test_kuka_interface.py
@@ -0,0 +1,24 @@
+import unittest
+
+from robot_dialogue_interface.kuka_dialogue_interface import \
+    KukaDialogueInterface
+
+
+class TestKukaInterface(unittest.TestCase):
+
+    def setUp(self):
+        self.interface = KukaDialogueInterface()
+
+    def test_demonstration_linkage(self):
+        # Make sure adding a demonstration in the dialogue interface also adds
+        # it to the correction publisher's internal store.
+
+        # Add a demonstration to the dialogue interface
+
+        # Make sure the correction publishder also has it.
+        #  TODO self.fail()
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
