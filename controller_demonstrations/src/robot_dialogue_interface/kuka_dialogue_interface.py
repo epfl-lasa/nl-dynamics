04520d4e0cb497277edb062caa8eaf834fccc3db
Felix Duvallet <felix.duvallet@epfl.ch>
2015-12-09 11:40:15 +0100

    kuka dialogue interface has a demonstration publisher.

--- controller_demonstrations/src/robot_dialogue_interface/kuka_dialogue_interface.py
+++ controller_demonstrations/src/robot_dialogue_interface/kuka_dialogue_interface.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 
+import roslib
 import rospy
-from robot_dialogue_interface.robot_dialogue_interface import \
-    RobotDialogueInterface
+
+from demonstrations.correction_publisher import PublishCorrections
+from robot_dialogue_interface import RobotDialogueInterface
 from nl_msgs.srv import Demonstration
 
 
@@ -11,8 +13,16 @@ class KukaDialogueInterface(RobotDialogueInterface):
         super(KukaDialogueInterface, self).__init__()
         self.srv = None
 
-    def connect(self):
+        # Create a correction publisher with no loaded corrections. The
+        # correction publisher handles 'anchoring' the desired correction using
+        # the current robot pose. Note that we link the _corrections variable
+        # name with _known_commands since they are storing the exact same data:
+        # changes to one will become changes to both.
+        self.correction_publisher = PublishCorrections(demonstration_dir=None)
+        # link names: python is awesome.
+        self.correction_publisher._corrections = self._known_commands
 
+    def connect(self):
         rospy.wait_for_service('Correction_Isolation')
         self.srv = rospy.ServiceProxy('Correction_Isolation', Demonstration)
         pass
@@ -29,9 +39,18 @@ class KukaDialogueInterface(RobotDialogueInterface):
         # Make sure the result is not stored.
         return False
 
+    def _robot_do_command(self, command, *args, **kwargs):
+
+        rospy.loginfo('Publishing command {} to Kuka.'.format(command))
+        self.correction_publisher.process_command(
+            nl_command=command, use_current_state_as_anchor=True)
+        rospy.loginfo('Finished publishing command.')
+        return True
+
     def _robot_set_speed(self, *args, **kwargs):
         rospy.loginfo('Unable to set speed at this time.')
 
 
 if __name__ == '__main__':
-    rospy.init_node('kuka_dialogue_interface')
+    rospy.init_node('kuka_dialogue_interface', anonymous=False)
+    kuka_interface = KukaDialogueInterface()
