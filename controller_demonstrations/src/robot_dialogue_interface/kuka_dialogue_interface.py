3131a27b92eeff2d712b257f483a53d33125a105
Felix Duvallet <felix.duvallet@epfl.ch>
2015-12-09 09:59:47 +0100

    Kuka interface sends a request and waits for a response.

--- controller_demonstrations/src/robot_dialogue_interface/kuka_dialogue_interface.py
+++ controller_demonstrations/src/robot_dialogue_interface/kuka_dialogue_interface.py
@@ -3,7 +3,8 @@
 import rospy
 from robot_dialogue_interface.robot_dialogue_interface import \
     RobotDialogueInterface
-from nl_msgs.srv import Demonstration
+from nl_msgs.srv import Demonstration, DemonstrationRequest
+
 
 
 class KukaDialogueInterface(RobotDialogueInterface):
@@ -17,13 +18,16 @@ class KukaDialogueInterface(RobotDialogueInterface):
         self.srv = rospy.ServiceProxy('Correction_Isolation', Demonstration)
         pass
 
-
     def _robot_record_command(self, command, *args, **kwargs):
         rospy.loginfo('Recording command: {}'.format(command))
 
+        response = self.srv(command)
 
+        if response.success:
+            return response.demonstration
 
-        pass
+        # Make sure the result is not stored.
+        return False
 
     def _robot_set_speed(self, *args, **kwargs):
         rospy.loginfo('Unable to set speed at this time.')
