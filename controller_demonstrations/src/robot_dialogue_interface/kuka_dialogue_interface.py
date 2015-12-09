6795453ecabcc1216c7933413a818e5c863a7b17
Felix Duvallet <felix.duvallet@epfl.ch>
2015-12-09 09:11:43 +0100

    Adds Kuka dialogue interface stubs.

---
+++ controller_demonstrations/src/robot_dialogue_interface/kuka_dialogue_interface.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import rospy
+from robot_dialogue_interface.robot_dialogue_interface import \
+    RobotDialogueInterface
+from nl_msgs.srv import Demonstration
+
+
+class KukaDialogueInterface(RobotDialogueInterface):
+    def __init__(self):
+        super(KukaDialogueInterface, self).__init__()
+        self.srv = None
+
+    def connect(self):
+
+        rospy.wait_for_service('Correction_Isolation')
+        self.srv = rospy.ServiceProxy('Correction_Isolation', Demonstration)
+        pass
+
+
+    def _robot_record_command(self, command, *args, **kwargs):
+        rospy.loginfo('Recording command: {}'.format(command))
+
+
+
+        pass
+
+    def _robot_set_speed(self, *args, **kwargs):
+        rospy.loginfo('Unable to set speed at this time.')
+
+
+if __name__ == '__main__':
+    rospy.init_node('kuka_dialogue_interface')
